d17198e09e8d9f56ff9834621c46a54c181e61c4
김용민 <dydals3440@gmail.com>
2024-07-17 19:47:04 +0900

    feat: Button Common Component 구현

--- src/screens/moim/MoimDetailScreen.tsx
+++ src/screens/moim/MoimDetailScreen.tsx
@@ -1,12 +1,17 @@
-import { SafeAreaView } from 'react-native';
+import { useState } from 'react';
+import { SafeAreaView, Text } from 'react-native';
 import Avatar from '@/components/common/Avatar';
+import Button from '@/components/common/Button';
 import CircleIconButton from '@/components/common/CircleIconButton';
 
 function MoimDetailScreen() {
 	return (
 		<SafeAreaView className="flex-1 bg-light-common-white dark:bg-light-common-black">
 			<CircleIconButton family={'MaterialIcons'} name={'drafts'} />
-			<Avatar size="lg" />
+			<Avatar size="md" />
+			<Button type="main" size="lg">
+				<Text>HI</Text>
+			</Button>
 		</SafeAreaView>
 	);
 }
